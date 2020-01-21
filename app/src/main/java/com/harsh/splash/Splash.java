ef554d7c09bb05481b7fdbc8101ed8f529ff38db
Harsh Tyagi <48311361+HarshTyagi007@users.noreply.github.com>
2020-01-21 09:57:10 +0530

    Our Team's Splash Screen For Android

---
+++ app/src/main/java/com/harsh/splash/Splash.java
@@ -0,0 +1,25 @@
+package com.harsh.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+    private ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo=(ImageView)findViewById(R.id.imageView);
+
+        Animation myanim = AnimationUtils.loadAnimation(Splash.this,R.anim.shade);
+        logo.startAnimation(myanim);
+
+
+    }
+}
